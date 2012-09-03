55f54f08ab11ad0652f7073c4c106490b4a8bdb7
Mike Meier <sk.mykon@gmail.com>
2012-09-03 19:43:22 +0200

    Added registerContainerParametersRecursive Method for setting Parameters for Services

--- DependencyInjection/IbrowsCodebaseApiExtension.php
+++ DependencyInjection/IbrowsCodebaseApiExtension.php
@@ -19,7 +19,29 @@ class IbrowsCodebaseApiExtension extends Extension
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
         
-        var_dump($config);
+        $alias = $this->getAlias();
+        $this->registerContainerParametersRecursive($container, $alias, $config);
+    }
+    
+    protected function registerContainerParametersRecursive(ContainerBuilder $container, $alias, $config)
+    {
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveArrayIterator($config), 
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+        
+        foreach($iterator as $value){
+            if($iterator->hasChildren()){
+                continue;
+            }
+            
+            $path = array();
+            for($i = 0; $i <= $iterator->getDepth(); $i++){
+                $path[] = $iterator->getSubIterator($i)->key();
+            }
+            
+            $container->setParameter($alias.'.'.implode(".", $path), $value);
+        }
     }
     
 }
